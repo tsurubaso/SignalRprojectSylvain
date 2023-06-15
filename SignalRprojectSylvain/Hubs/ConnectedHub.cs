0855db86f14928db76b9cef0f27df94575fb75f6
tsurubaso <tsurubaso@hotmail.com>
2023-06-15 10:39:09 +0900

    Add project files.

---
+++ SignalRprojectSylvain/Hubs/ConnectedHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace SignalRprojectSylvain.Hubs
+{
+    public class ConnectedHub: Hub
+    {
+
+        //Ctrl + K, Ctrl + D
+
+        public async Task SendMessage(string user, string message)
+        {
+
+
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
